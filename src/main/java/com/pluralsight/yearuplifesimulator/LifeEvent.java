8dcc5e0955b3baae9542c2223f302998df0528e7
kavonraggs <kavonraggs@gmail.com>
2025-10-18 16:41:27 -0500

    events JSON file created

--- src/main/java/com/pluralsight/yearuplifesimulator/LifeEvent.java
+++ src/main/java/com/pluralsight/yearuplifesimulator/LifeEvent.java
@@ -2,6 +2,7 @@ package com.pluralsight.yearuplifesimulator;
 
 
 public class LifeEvent {
+    private final String message;
     private final String vendor;
     private final String description;
     private final double amount;
@@ -9,11 +10,16 @@ public class LifeEvent {
     public enum EventType { DEPOSIT, PAYMENT }
 
 
-    public LifeEvent(String vendor, String description, double amount, EventType type){
+    public LifeEvent(String message, String vendor, String description, double amount, EventType type){
+        this.message = message;
         this.vendor = vendor;
         this.description = description;
         this.amount = amount;
         this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
     }
 
     public EventType getType() {
